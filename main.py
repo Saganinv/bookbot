65be26de0163d53d73b754332797068cb9b3ff32
Saganinv <saganinvestments@gmail.com>
2025-02-28 09:12:30 +1300

    Printout added

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from stats import get_num_words, get_chars_dict, sort_on_char
+from stats import get_num_words, sort_on_char
 
 
 def main():
     book_path = "/home/sam/workspace/github.com/Saganinv/bookbot/books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    chars_dict = get_chars_dict(text)
     sorted_dict = sort_on_char(text)
-    print(sorted_dict)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for char in sorted_dict:
+        if char.isalpha():
+            print(f"{char}: {sorted_dict[char]}")
+    print("============= END ===============")
 
 
 def get_book_text(path):
