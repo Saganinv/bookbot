c02d7a20fbc630ab18589e1d03333196dcefa483
Saganinv <saganinvestments@gmail.com>
2025-02-28 08:31:25 +1300

    Refactor code

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from stats import get_word_count
-from stats import get_char_count
+from stats import get_num_words, get_chars_dict
 
-def main(file):
-    word_count = get_word_count(file)
-    char_count = get_char_count(file)
-    print(f"{word_count} words found in the document")
-    print(char_count)
 
-main("/home/sam/workspace/github.com/Saganinv/bookbot/books/frankenstein.txt")
+def main():
+    book_path = "/home/sam/workspace/github.com/Saganinv/bookbot/books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    print(f"{num_words} words found in the document")
+    print(chars_dict)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
--- stats.py
+++ stats.py
@@ -1,42 +1,14 @@
-def get_book_txt(path_to_file):
-    with open(path_to_file) as f:
-        file_contents = f.read()
-    return file_contents
-
-def get_word_count(file):
-    book_text = get_book_txt(file)
-    words = book_text.split()
+def get_num_words(text):
+    words = text.split()
     return len(words)
 
-def get_char_count(file):
-    text = get_book_txt(file)
-    lowecase_text = text.lower()
-    character_count = {
-        "a": lowecase_text.count("a"),
-        "b": lowecase_text.count("b"),
-        "c": lowecase_text.count("c"),
-        "d": lowecase_text.count("d"),
-        "e": lowecase_text.count("e"),
-        "f": lowecase_text.count("f"),
-        "g": lowecase_text.count("g"),
-        "h": lowecase_text.count("h"),
-        "i": lowecase_text.count("i"),
-        "j": lowecase_text.count("j"),
-        "k": lowecase_text.count("k"),
-        "l": lowecase_text.count("l"),
-        "m": lowecase_text.count("m"),
-        "n": lowecase_text.count("n"),
-        "o": lowecase_text.count("o"),
-        "p": lowecase_text.count("p"),
-        "q": lowecase_text.count("q"),
-        "r": lowecase_text.count("r"),
-        "s": lowecase_text.count("s"),
-        "t": lowecase_text.count("t"),
-        "u": lowecase_text.count("u"),
-        "v": lowecase_text.count("v"),
-        "w": lowecase_text.count("w"),
-        "x": lowecase_text.count("x"),
-        "y": lowecase_text.count("y"),
-        "z": lowecase_text.count("z"),
-    }
-    return character_count
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
