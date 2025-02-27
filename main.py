6b8f2004850a3e74e5a0db1453a6ec8c2a4d3ef7
Saganinv <saganinvestments@gmail.com>
2025-02-28 08:03:20 +1300

    Character count implemented

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from stats import get_word_count
-
-def get_book_txt(path_to_file):
-    with open(path_to_file) as f:
-        file_contents = f.read()
-    return file_contents
+from stats import get_char_count
 
 def main(file):
     word_count = get_word_count(file)
+    char_count = get_char_count(file)
     print(f"{word_count} words found in the document")
+    print(char_count)
 
 main("/home/sam/workspace/github.com/Saganinv/bookbot/books/frankenstein.txt")
